7448551af5c7ca323dee4ae47a47a06da96cd09d
Ethan <engerethan@gmail.com>
2023-06-01 23:15:37 -0600

    Final Submission. Best I could do

--- EthanEngerExcercises.js
+++ EthanEngerExcercises.js
@@ -25,26 +25,48 @@ document.addEventListener("load", () =>    //Make sure to not block loading or r
 
 //Requirement 2
 function ReadJumpIdThenStoreThisSpinLocks() {
-    let url = document.URL
-    //let regex = new RegExp("jumpid=") //I couldn't figure out the regex.
-    let jumpid = url.split("jumpid=")[1].split("#")[0];
-    while (digitalData.page.pageInfo.jumpid == undefined) { }//I am so sorry for my spin lock 
-    digitalData.page.pageInfo.jumpid = jumpid;
+    document.addEventListener("DOMContentLoaded", () =>
+    {
+        let url = document.URL
+        //let regex = new RegExp("/jumpid=[a-zA-Z0-9+_[a-zA-Z0-9]]") //I couldn't figure out the regex.
+        //lets figure t out
+        let jumpid = url.split("jumpid=")[1].split("#")[0];
+        //while (digitalData.page.pageInfo.jumpid == undefined) { }//I am so sorry for my spin lock 
+        digitalData.page.pageInfo.jumpid = jumpid; //Should be available when dom is loaded. Hopefully
+    });
 }
 
 //Requirement 3
 function MakeElementClickStoreage() {
     document.addEventListener("click", (e) => {
-        let clickArray = localStorage.getItem("click_activity") != null ? JSON.parse(localStorage.getItem("click_activity")) : [];
-        let log = e.target.tagName.toLowerCase(); //add tag
-        log += ":" + e.target.getAttribute("id"); //add attribute
-        let classes = e.target.getAttribute("class").replace(" ", "."); //make classes into correct format
-        log += ":" + classes;
-        log += ":" + e.target.tagName.toLowerCase();
-        clickArray.push(log);
-        localStorage.setItem("click_activity", clickArray);
+        AddClickActivity(e.target)
     });
+}
+
+function AddClickActivity(e)
+{
+    let clickArray = localStorage.getItem("click_activity") !== null ? JSON.parse(localStorage.getItem("click_activity")) : [];
+    let log = e.tagName.toLowerCase(); //add tag
+    log += ":" + e.getAttribute("id"); //add attribute
+    let classes = e.getAttribute("class").replace(" ", "."); //make classes into correct format
+    log += ":" + classes;
+    log += ":" + e.getAttribute("name");
+    clickArray.push(log);
+    localStorage.setItem("click_activity", clickArray);
 }
 
 //Requirement 4
 //Figure out test cases dummy
+//lets try mocha
+var assert = require('mocha');
+describe('Click Activity', () => {
+    describe('AddClickActivity()', () =>
+    {
+        let exampleElement = document.createElement("a")
+        exampleElement.setAttribute("id", "matt1");
+        exampleElement.setAttribute("class", "link button fixture");
+        exampleElement.setAttribute("name", "matt");
+        AddClickActivity(exampleElement);
+        assert.equal(JSON.parse(localStorage.getItem("click_activity")[0], "a:matt1:link.button.fixture:matt"))
+    });
+});
